49afd7d1f61ac4d0f8151568a103d3e513676dfe
mposso7 <micposso@gmail.com>
2017-12-12 13:11:18 -0500

    add state to note component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    //set react state of component
+    this.state = {
+      notes: [
+        {noteContent: "This is note 1", id: 1},
+        {noteContent: "This is note 2", id: 2}
+      ],
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      {
+        this.state.notes.map((note) => {
+          return (
+            <Note noteContent={note.noteContent} NoteId={note.id} key={note.id} />            
+          )
+        })
+      }
       </div>
     );
   }
